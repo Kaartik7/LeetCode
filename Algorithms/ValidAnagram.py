2e0b730be5c3b9e473bdfde9f3a496370ee5346f
Kaartik7 <50298143+Kaartik7@users.noreply.github.com>
2022-06-04 14:57:20 -0400

    Add files via upload

---
+++ Algorithms/ValidAnagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d1 = dict()
+        d2 = dict()
+        for i in s:
+            d1[i] = 0
+        for i in s:
+            d1[i] = d1[i] + 1
+        for i in t:
+            d2[i] = 0
+        for i in t:
+            d2[i] = d2[i] + 1
+        for i in s:
+            if i not in d2 or d1[i] != d2[i]:
+                return False
+
+        return True
